7361d3e457b15db19fccd20dc308a33954d329d9
Amulya Pervaje <amulyapervaje@gmail.com>
2018-07-10 11:58:34 -0400

    listtools.py

--- sssdevops/listtools.py
+++ sssdevops/listtools.py
@@ -21,9 +21,5 @@ def split(in_list,index):
 	>>> split(['a', 'b', 'c', 'd'],3)
 	(['a', 'b', 'c'], ['d'])
 	"""
-	
-	list1 = in_list[:index]
-	list2 = in_list[index:]
-
-	return list1, list2
+	pass	
 
