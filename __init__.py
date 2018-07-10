f33a20b088d5e3998f49a324a25593d213c93df2
Amulya Pervaje <amulyapervaje@gmail.com>
2018-07-10 10:14:54 -0400

    modifying __init__.py so that test can run

--- __init__.py
+++ __init__.py
@@ -5,6 +5,4 @@ Main init file for the sssdevops package
 from .sssdevops import *
 from .listtools import *
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+
